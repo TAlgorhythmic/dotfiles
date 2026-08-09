51ba3224d783a2f3aa61fc9203f4d93d39dbf60c
Algorhythmics <intivioli@gmail.com>
2026-08-09 17:56:09 +0200

    Remove eww

--- eww/scripts/vol.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Volume actions for the bar (scroll + click-to-mute).
-case "${1:-}" in
-    up)   wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+ ;;
-    down) wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- ;;
-    mute) wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle ;;
-esac
